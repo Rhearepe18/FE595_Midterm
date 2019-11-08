04cd349cf2d19fa37093c4bf9102ea2f415ccf8c
Asna Fatima Ali <aali6@stevens.edu>
2019-11-08 06:59:51 -0500

    adding routes for services

--- midterm_service.py
+++ midterm_service.py
@@ -1,5 +1,5 @@
 from flask import Flask, escape, request, Response, render_template, redirect, url_for
-from fuzzywuzzy import fuzz
+#from fuzzywuzzy import fuzz
 from imdb import IMDb
 import json
 import re
@@ -57,89 +57,238 @@ def movie_selection(user_input):
     movie_info = info_cleaner(movie_info)
     return movie_info
 
-
-@app.route('/homepage', methods =['GET', 'POST'])
-def homepage():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
-
-
-
-@app.route('/service1', methods = ['GET'])
-def get_sentiment():
-    name = request.args.get("name", "Service1")
-    text = str(movie_info[0])
-    blob = TextBlob(text)
-    blob.tags
-    blob.noun_phrases
-    
-    for sentence in blob.sentences:
-        text_sentiment = sentence.sentiment.polarity
-    
-    return (text_sentiment)
-
-print(get_sentiment(text))
-
-@app.route('/service2', methods = ['GET'])
-def get_key(val):
-    name = request.args.get("name", "Service2")
-    
-    for key, value in language.items(): 
-         if val == value: 
-            return key
-    return ("key doesn't exist") 
-   
 language = dict([('ab', 'Abkhaz'),
-('aa', 'Afar'),('af', 'Afrikaans'),('ak', 'Akan'),('sq', 'Albanian'),('am', 'Amharic'),('ar', 'Arabic'),
-('an', 'Aragonese'),('hy', 'Armenian'),('as', 'Assamese'),('av', 'Avaric'),('ae', 'Avestan'),('ay', 'Aymara'),
-('az', 'Azerbaijani'),('bm', 'Bambara'),('ba', 'Bashkir'),('eu', 'Basque'),('be', 'Belarusian'),
-('bn', 'Bengali'),('bh', 'Bihari'),('bi', 'Bislama'),('bs', 'Bosnian'),('br', 'Breton'),('bg', 'Bulgarian'),
-('my', 'Burmese'),('ca', 'Catalan; Valencian'),('ch', 'Chamorro'),('ce', 'Chechen'),('ny', 'Chichewa; Chewa; Nyanja'),
-('zh', 'Chinese'),('cv', 'Chuvash'),('kw', 'Cornish'),('co', 'Corsican'),('cr', 'Cree'),('hr', 'Croatian'),
-('cs', 'Czech'),('da', 'Danish'),('dv', 'Divehi; Maldivian;'),('nl', 'Dutch'),('dz', 'Dzongkha'),
-('en', 'English'),('eo', 'Esperanto'),('et', 'Estonian'),('ee', 'Ewe'),('fo', 'Faroese'),('fj', 'Fijian'),
-('fi', 'Finnish'),('fr', 'French'),('ff', 'Fula'),('gl', 'Galician'),('ka', 'Georgian'),('de', 'German'),
-('el', 'Greek, Modern'),('gn', 'Guaraní'),('gu', 'Gujarati'),('ht', 'Haitian'),('ha', 'Hausa'),('he', 'Hebrew (modern)'),
-('hz', 'Herero'),('hi', 'Hindi'),('ho', 'Hiri Motu'),('hu', 'Hungarian'),('ia', 'Interlingua'),('id', 'Indonesian'),
-('ie', 'Interlingue'),('ga', 'Irish'),('ig', 'Igbo'),('ik', 'Inupiaq'),('io', 'Ido'),('is', 'Icelandic'),
-('it', 'Italian'),('iu', 'Inuktitut'),('ja', 'Japanese'),('jv', 'Javanese'),('kl', 'Kalaallisut'),('kn', 'Kannada'),
-('kr', 'Kanuri'),('ks', 'Kashmiri'),('kk', 'Kazakh'),('km', 'Khmer'),('ki', 'Kikuyu, Gikuyu'),('rw', 'Kinyarwanda'),
-('ky', 'Kirghiz, Kyrgyz'),('kv', 'Komi'),('kg', 'Kongo'),('ko', 'Korean'),('ku', 'Kurdish'),('kj', 'Kwanyama, Kuanyama'),
-('la', 'Latin'),('lb', 'Luxembourgish'),('lg', 'Luganda'),('li', 'Limburgish'),('ln', 'Lingala'),('lo', 'Lao'),
-('lt', 'Lithuanian'),('lu', 'Luba-Katanga'),('lv', 'Latvian'),('gv', 'Manx'),('mk', 'Macedonian'),('mg', 'Malagasy'),
-('ms', 'Malay'),('ml', 'Malayalam'),('mt', 'Maltese'),('mi', 'Māori'),('mr', 'Marathi (Marāṭhī)'),('mh', 'Marshallese'),
-('mn', 'Mongolian'),('na', 'Nauru'),('nv', 'Navajo, Navaho'),('nb', 'Norwegian Bokmål'),('nd', 'North Ndebele'),
-('ne', 'Nepali'),('ng', 'Ndonga'),('nn', 'Norwegian Nynorsk'),('no', 'Norwegian'),('ii', 'Nuosu'),('nr', 'South Ndebele'),
-('oc', 'Occitan'),('oj', 'Ojibwe, Ojibwa'),('cu', 'Old Church Slavonic'),('om', 'Oromo'),('or', 'Oriya'),
-('os', 'Ossetian, Ossetic'),('pa', 'Panjabi, Punjabi'),('pi', 'Pāli'),('fa', 'Persian'),('pl', 'Polish'),
-('ps', 'Pashto, Pushto'),('pt', 'Portuguese'),('qu', 'Quechua'),('rm', 'Romansh'),('rn', 'Kirundi'),
-('ro', 'Romanian, Moldavan'),('ru', 'Russian'),('sa', 'Sanskrit (Saṁskṛta)'),('sc', 'Sardinian'),('sd', 'Sindhi'),
-('se', 'Northern Sami'),('sm', 'Samoan'),('sg', 'Sango'),('sr', 'Serbian'),('gd', 'Scottish Gaelic'),('sn', 'Shona'),
-('si', 'Sinhala, Sinhalese'),('sk', 'Slovak'),('sl', 'Slovene'),('so', 'Somali'),('st', 'Southern Sotho'),('es', 'Spanish; Castilian'),
-('su', 'Sundanese'),('sw', 'Swahili'),('ss', 'Swati'),('sv', 'Swedish'),('ta', 'Tamil'),('te', 'Telugu'),('tg', 'Tajik'),
-('th', 'Thai'),('ti', 'Tigrinya'),('bo', 'Tibetan'),('tk', 'Turkmen'),('tl', 'Tagalog'),('tn', 'Tswana'),('to', 'Tonga'),
-('tr', 'Turkish'),('ts', 'Tsonga'),('tt', 'Tatar'),('tw', 'Twi'),('ty', 'Tahitian'),('ug', 'Uighur, Uyghur'),
-('uk', 'Ukrainian'),('ur', 'Urdu'),('uz', 'Uzbek'),('ve', 'Venda'),('vi', 'Vietnamese'),('vo', 'Volapük'),
-('wa', 'Walloon'),('cy', 'Welsh'),('wo', 'Wolof'),('fy', 'Western Frisian'),('xh', 'Xhosa'),('yi', 'Yiddish'),
-('yo', 'Yoruba'),('za', 'Zhuang, Chuang'),('zu', 'Zulu')])
+    ('aa', 'Afar'),('af', 'Afrikaans'),('ak', 'Akan'),('sq', 'Albanian'),('am', 'Amharic'),('ar', 'Arabic'),
+    ('an', 'Aragonese'),('hy', 'Armenian'),('as', 'Assamese'),('av', 'Avaric'),('ae', 'Avestan'),('ay', 'Aymara'),
+    ('az', 'Azerbaijani'),('bm', 'Bambara'),('ba', 'Bashkir'),('eu', 'Basque'),('be', 'Belarusian'),
+    ('bn', 'Bengali'),('bh', 'Bihari'),('bi', 'Bislama'),('bs', 'Bosnian'),('br', 'Breton'),('bg', 'Bulgarian'),
+    ('my', 'Burmese'),('ca', 'Catalan; Valencian'),('ch', 'Chamorro'),('ce', 'Chechen'),('ny', 'Chichewa; Chewa; Nyanja'),
+    ('zh', 'Chinese'),('cv', 'Chuvash'),('kw', 'Cornish'),('co', 'Corsican'),('cr', 'Cree'),('hr', 'Croatian'),
+    ('cs', 'Czech'),('da', 'Danish'),('dv', 'Divehi; Maldivian;'),('nl', 'Dutch'),('dz', 'Dzongkha'),
+    ('en', 'English'),('eo', 'Esperanto'),('et', 'Estonian'),('ee', 'Ewe'),('fo', 'Faroese'),('fj', 'Fijian'),
+    ('fi', 'Finnish'),('fr', 'French'),('ff', 'Fula'),('gl', 'Galician'),('ka', 'Georgian'),('de', 'German'),
+    ('el', 'Greek, Modern'),('gn', 'Guaraní'),('gu', 'Gujarati'),('ht', 'Haitian'),('ha', 'Hausa'),('he', 'Hebrew (modern)'),
+    ('hz', 'Herero'),('hi', 'Hindi'),('ho', 'Hiri Motu'),('hu', 'Hungarian'),('ia', 'Interlingua'),('id', 'Indonesian'),
+    ('ie', 'Interlingue'),('ga', 'Irish'),('ig', 'Igbo'),('ik', 'Inupiaq'),('io', 'Ido'),('is', 'Icelandic'),
+    ('it', 'Italian'),('iu', 'Inuktitut'),('ja', 'Japanese'),('jv', 'Javanese'),('kl', 'Kalaallisut'),('kn', 'Kannada'),
+    ('kr', 'Kanuri'),('ks', 'Kashmiri'),('kk', 'Kazakh'),('km', 'Khmer'),('ki', 'Kikuyu, Gikuyu'),('rw', 'Kinyarwanda'),
+    ('ky', 'Kirghiz, Kyrgyz'),('kv', 'Komi'),('kg', 'Kongo'),('ko', 'Korean'),('ku', 'Kurdish'),('kj', 'Kwanyama, Kuanyama'),
+    ('la', 'Latin'),('lb', 'Luxembourgish'),('lg', 'Luganda'),('li', 'Limburgish'),('ln', 'Lingala'),('lo', 'Lao'),
+    ('lt', 'Lithuanian'),('lu', 'Luba-Katanga'),('lv', 'Latvian'),('gv', 'Manx'),('mk', 'Macedonian'),('mg', 'Malagasy'),
+    ('ms', 'Malay'),('ml', 'Malayalam'),('mt', 'Maltese'),('mi', 'Māori'),('mr', 'Marathi (Marāṭhī)'),('mh', 'Marshallese'),
+    ('mn', 'Mongolian'),('na', 'Nauru'),('nv', 'Navajo, Navaho'),('nb', 'Norwegian Bokmål'),('nd', 'North Ndebele'),
+    ('ne', 'Nepali'),('ng', 'Ndonga'),('nn', 'Norwegian Nynorsk'),('no', 'Norwegian'),('ii', 'Nuosu'),('nr', 'South Ndebele'),
+    ('oc', 'Occitan'),('oj', 'Ojibwe, Ojibwa'),('cu', 'Old Church Slavonic'),('om', 'Oromo'),('or', 'Oriya'),
+    ('os', 'Ossetian, Ossetic'),('pa', 'Panjabi, Punjabi'),('pi', 'Pāli'),('fa', 'Persian'),('pl', 'Polish'),
+    ('ps', 'Pashto, Pushto'),('pt', 'Portuguese'),('qu', 'Quechua'),('rm', 'Romansh'),('rn', 'Kirundi'),
+    ('ro', 'Romanian, Moldavan'),('ru', 'Russian'),('sa', 'Sanskrit (Saṁskṛta)'),('sc', 'Sardinian'),('sd', 'Sindhi'),
+    ('se', 'Northern Sami'),('sm', 'Samoan'),('sg', 'Sango'),('sr', 'Serbian'),('gd', 'Scottish Gaelic'),('sn', 'Shona'),
+    ('si', 'Sinhala, Sinhalese'),('sk', 'Slovak'),('sl', 'Slovene'),('so', 'Somali'),('st', 'Southern Sotho'),('es', 'Spanish; Castilian'),
+    ('su', 'Sundanese'),('sw', 'Swahili'),('ss', 'Swati'),('sv', 'Swedish'),('ta', 'Tamil'),('te', 'Telugu'),('tg', 'Tajik'),
+    ('th', 'Thai'),('ti', 'Tigrinya'),('bo', 'Tibetan'),('tk', 'Turkmen'),('tl', 'Tagalog'),('tn', 'Tswana'),('to', 'Tonga'),
+    ('tr', 'Turkish'),('ts', 'Tsonga'),('tt', 'Tatar'),('tw', 'Twi'),('ty', 'Tahitian'),('ug', 'Uighur, Uyghur'),
+    ('uk', 'Ukrainian'),('ur', 'Urdu'),('uz', 'Uzbek'),('ve', 'Venda'),('vi', 'Vietnamese'),('vo', 'Volapük'),
+    ('wa', 'Walloon'),('cy', 'Welsh'),('wo', 'Wolof'),('fy', 'Western Frisian'),('xh', 'Xhosa'),('yi', 'Yiddish'),
+    ('yo', 'Yoruba'),('za', 'Zhuang, Chuang'),('zu', 'Zulu')])
+
+# These exact stop words are from the NLTK stopwords
+stopwords_list = ['a','about','above','after','again','against','ain','all','am','an','and',
+'any','are','aren',"aren't",'as','at','be','because','been','before','being','below',
+'between','both','but','by','can','couldn',"couldn't",'d','did','didn',"didn't",
+'do','does','doesn', "doesn't",'doing','don',"don't",'down','during','each',
+'few','for','from','further','had','hadn',"hadn't",'has','hasn',"hasn't",'have',
+'haven',"haven't",'having','he','her','here','hers','herself','him','himself',
+'his','how','i','if','in','into','is','isn',"isn't",'it',"it's",'its','itself',
+'just','ll','m','ma','me','mightn',"mightn't",'more','most','mustn',"mustn't",
+'my','myself','needn',"needn't",'no','nor','not','now','o','of','off','on',
+'once','only','or','other','our','ours','ourselves','out','over','own','re',
+'s','same','shan',"shan't",'she',"she's",'should',"should've",'shouldn',"shouldn't",
+'so','some','such','t','than','that',"that'll",'the','their','theirs','them','themselves',
+'then','there','these','they','this','those','through','to','too','under','until',
+'up','ve','very','was','wasn',"wasn't",'we','were','weren',"weren't",'what','when',
+'where','which','while','who','whom','why','will','with','won',"won't",'wouldn',
+"wouldn't",'y','you',"you'd","you'll","you're","you've",'your','yours','yourself',
+'yourselves']
 
 
-@app.route('/service3', methods = ['GET'])
-def final_text(blob):
-    name = request.args.get("name", "Service3")
+happy_list = ["overjoy","cheerful", "happy","content", "delighted", "delight","ecstatic", "elated","glad", "joyful","joyous","joy", 
+"jubilant", "lively", "merry","overjoyed","peaceful","pleasant", "pleased","thrilled", "upbeat","blessed","blest","blissful", "blithe",
+"captivated", "chipper", "chirpy","convivial", "exultant", "gay","gleeful", "gratified","intoxicated", "light", "peppy","perky", "sparkling", 
+"sunny","tickled", "up", "satisfy"]
+happy_list = " ".join(happy_list)
+
+sad_list = ["bitter", "dismal", "melancholy", "mournful", "somber","wistful", "low", 
+"morose", "bereaved", "wistful", "sorry", "doleful", "heartsick", "hurting","gloomy", "blue","weeping"
+"dejected","sad", "irritate","lousy","upset","incapable","disappointment","doubtful","alone",
+"hostile","discourage","uncertain","insult","ashame","indecisive","fatigue","sore","powerless",
+"perplex","useless","annoy","diminish","embarrass","inferior","upset","guilty","hesitant","vulnerable",
+"hateful","dissatisfy","shy","empty","unpleasant","miserable","stupefied","forced","offensive","detestable",
+"disillusion","hesitant","bitter","repugnant","unbelieving","despair","aggressive","despicable","skeptical",
+"frustrated","resentful","distress","inflame","abominable","misgiving","woeful","terrible","lost","pathetic","incensed","indespair","unsure","tragic","infuriate","sulky","uneasy",
+"cross","dominate","tense","boil","insensitive","fearful","tearful","dull","terrified","torment",
+"sorrowful","nonchalant","deprive","neutral","anxious","pain","grief","reserve",
+"anguish","weary","deject","desolate","bore","nervous","reject","desperate","preoccupied","injure",
+"pessimistic","worry","offend","unhappy","disinterest","afflict","lonely","lifeless","timid","ache",
+"grieve","shaky","victim","mourn","restless","heartbroken","dismay","doubt","agony","threaten","coward","humiliate","alienate","wary"]
+sad_list = " ".join(sad_list)
+
+scary_list = ["alarm", "torture", "morbid", "tragic", "distrustful", "enraged", "provoke", "bad", "alarming", "chilling", "creepy", 
+"eerie", "horrifying", "horrify", "intimidating", "shocking", "spooky", "shock", "bloddcurdling", "panic"
+"horrendous", "gore", "unnerving" "frightening",  "daunting", "blood", "carnage", "slaughter", "evil", "paralyze", "dark"]
+scary_list = " ".join(scary_list)
+
+romantic_list = ["amorous", "charming", "corny", "dreamy", "erotic", "exciting", "exotic", "fanciful", "glamorous", "passionate", "tender", 
+"chivalrous", "fond", "pituresque", "loving", "idyllic", 'heart','lovely', 'family','caring','forever','trust','passion','romance','sweet',
+'kiss','love','hugs','warm','fun','kisses','joy','friendship','marriage','husband','wife','forever']
+romantic_list = " ".join(romantic_list)
+
+mystery_list =["suspicious", "baffling", "cryptic", "curious", "enigmatic", "inexplicable", "mystical", "obscure", "perplexing", "puzzling", "mysterious", "mystery"
+"secretive", "weird", "abstruse", "arcane", "covert", "hidden", "impenetrable", "strange", "unknown", "insoluble", "incomprehensible", "furtive", "difficult", 
+"necromantic", "occult", "oracular", "recondite", 'spiritual', "subjective", "symbolic", 'uncanny', "transcendental", "unfathomable", "unknowable", "unnatural", "veiled"]
+mystery_list = " ".join(mystery_list)
+
+comedy_list = ["absurd", "amusing", "droll", "entertaining", "hilarious", "ludicrous", "playful", "ridiculous", "silly", "whimsical", "antic", "slapstick", "farcical",
+"jolly", "laughable", "mirthful", "priceless", "riotous", "waggish", "witty", "joke", "comedy", "comedic", "comedians"]
+comedy_list = " ".join(comedy_list)
+
+vectorization = TfidfVectorizer()
+
+def cosine_similarity(input1, input2):
+    tfidf = vectorization.fit_transform([input1, input2])
+    return ((tfidf * tfidf.T).A)[0,1]
+
+@app.route('/', methods =['GET', 'POST'])
+def homepage():
+    return render_template('services.html')
+
+
+@app.route('/service1', methods = ["GET", "POST"])
+def service1():
+    render_template('one_input.html')
+    if request.method == "POST":
+        return redirect(url_for('/service1_result'))
+    else:
+        return render_template('one_input.html')
+
+@app.route('/service1_result', methods = ['GET', 'POST'])
+def service1_result():
+    user_input1 = request.form['text']
+
+    def get_sentiment(user_movie_input):
+        text = movie_selection(user_movie_input)
+        blob = TextBlob(text)
+        blob.tags
+        blob.noun_phrases
+        
+        for sentence in blob.sentences:
+            text_sentiment = sentence.sentiment.polarity
+        
+        return (text_sentiment)
+    movie_sentiment = get_sentiment(user_input1)
+    return render_template('present.html', output = movie_sentiment)
+
+
+@app.route('/service2',methods = ['GET', "POST"])
+def service2():
+    render_template('two_input.html')
+    if request.method == "POST":
+        return redirect(url_for('/service2_result'))
+    else:
+        return render_template('two_input.html')
+
+@app.route('/service2_result', methods = ['GET', "POST"])
+def service2_result():
+    user_input1 = request.form['text']
+    user_input2 = request.form['text2']
+
+    def get_key(val):
+        for key, value in language.items(): 
+            if val == value:
+                lang = key
+                break
+            else:
+                lang = "Cannot Translate Synopsis"
+        return lang
     
-    enter_language=input('enter a language')
-    chosen_language = get_key(enter_language)
-    blob = TextBlob(str(text))
+    def final_text(user_movie_input, chosen_language):
+        chosen_language = get_key(chosen_language)
+        if chosen_language == "Cannot Translate Synopsis":
+            translation = chosen_language
+        else:
+            blob = TextBlob(movie_selection(user_movie_input))
+            translation = blob.translate(to = chosen_language)
+        return(translation)
+    return render_template('present.html', output = final_text(user_input1, user_input2))
+
+@app.route('/service3', methods = ['GET', 'POST'])
+def service3():
+    render_template('two_input.html')
+    if request.method == "POST":
+        return redirect(url_for('/service3_result'))
+    else:
+        return render_template('two_input.html')
+
+@app.route('.service3_result', methods = ['GET', 'POST'])
+def service3_result():
+    user_input1 = request.form['text']
+    user_input2 = request.form['text2']
     
-    return (blob.translate(to = chosen_language))
+    def lemmatize_sentence(input_string):
+        lemma_sentence = ""
+        lemmas = []
+        input_string = TextBlob(input_string)
+        for x in input_string.words:
+            x = Word(x)
+            lemmas.append(x.lemmatize())
+        for lemma in lemmas:
+            if lemma not in stopwords_list:
+                lemma_sentence = lemma_sentence + " " + lemma
+        lemma_sentence = re.sub(r" \'", "\'", lemma_sentence)
+        lemma_sentence = re.sub(r"^[ \t]+|[ \t]+$", "", lemma_sentence)
 
-print(final_text(blob))
+        return lemma_sentence
 
-@app.route('/service4', methods = ['GET'])
+    movie1 = lemmatize_sentence(movie_selection(user_input))
+    movie2 = lemmatize_sentence(movie_selection(user_input2))
+
+    score = cosine_similarity(movie1, movie2)
+    score = str(score)
+    return render_template('present.html', output = score)
+
+
+@app.route('/service4', methods = ['GET', 'POST'])
 def service4():
-    name = request.args.get("name", "Service4")
-    return f'Hello, {escape(name)}!'
+    render_template('two_input.html')
+    if request.method == "POST":
+        return redirect(url_for('/service4_result'))
+    else:
+        return render_template('two_input.html')
+
+
+@app.route('/service4_result', methods = ['GET', ' POST'])
+def service4_result():
+    user_input1 = request.form['text']
+    user_input2 = request.form['text2']
+
+    def mood_similarity(user_movie_input, mood_input):
+        if mood_input in happy_list:
+            service_output = "The cosine score of this being a happy movie is {}".format(cosine_similarity(user_movie_input, happy_list))
+        elif mood_input in sad_list:
+            service_output = "The cosine score of this being a sad movie is {}".format(cosine_similarity(user_movie_input, sad_list))
+        elif mood_input in scary_list:
+            service_output = "The cosine score of this being a scary movie is {}".format(cosine_similarity(user_movie_input, scary_list))
+        elif mood_input in mystery_list:
+            service_output = "The cosine score of this being a mysterious movie is {}".format(cosine_similarity(user_movie_input, mystery_list))
+        elif mood_input in romantic_list:
+            service_output = "The cosine score of this being a romantic movie is {}".format(cosine_similarity(user_movie_input, romantic_list))
+        elif mood_input in comedy_list:
+            service_output = "The cosine score of this being a comedic movie is {}".format(cosine_similarity(user_movie_input, comedy_list))
+        return(service_output)
+
+    mood_score = mood_similarity(user_input1, user_input2)
+
+    return render_template('present.html', output = mood_score)
+
 
 
 @app.route('/service5', methods = ['GET'])
